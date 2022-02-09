841b9d17328ca178254fa2455f6071bcad7aca3b
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-02-09 11:02:37 +0300

    Create Post controller & seeder & migration and route

---
+++ src/database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'title' => 'Example title',
+            'content' => Str::random(200)
+        ]);
+    }
+}
